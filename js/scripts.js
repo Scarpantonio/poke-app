8a0f3f4bdb4e00e99267e16089a4d2ecd09d4277
scarpantonioc <scarpantonioc@gmail.com>
2020-05-30 06:28:34 -0600

    Adding an array of pokemonList
    added 3 objets with pokemon descriptions to the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
-// alert('hello world')
 
-var favoriteFood = "Pizza";
-document.write(favoriteFood)
+var pokemonList = [
+    {
+        name:'Bulbasaur',
+        height: 7,
+        type=['grass','poison']
+    },
+    {
+        name:'Venusaur',
+        height: 2,
+        type=['grass','poison']
+    },
+    {
+        name:'charizard',
+        height: 1.7,
+        type=['fire','flying']
+    }
+]
+
