c00e81755cb7a453da52ce2e6cfa59ab1a105fe2
scarpantonioc <scarpantonioc@gmail.com>
2020-06-03 10:37:06 -0600

    Adding Modals to poke Details

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,57 @@ var pokemonRepository = (() => {
         return pokemonList;
     }
 
+    // Hide modal
+    function hideModal() {
+        var modalContainer = document.querySelector('#modal-container');
+        modalContainer.classList.remove('is-visible');
+    }
+
+    // to close modal - ESC key
+    window.addEventListener('keydown', (e) => {
+        var modalContainer = document.querySelector('#modal-container');
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+        hideModal();  
+        }
+    });
+
+    // Open modal
+    var showModal = (item) => {
+        var modalContainer = document.querySelector('#modal-container');
+      
+        // Clear all existing modal content
+        modalContainer.innerHTML = '';
+        modalContainer.classList.add('is-visible');
+      
+        var modal = document.createElement('div');
+        modal.classList.add('modal');
+      
+        // Add the new modal content
+        var closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'X';
+        closeButtonElement.addEventListener('click', hideModal);
+
+        // creating title in modal content
+        var titleElement = document.createElement('h1');
+        titleElement.classList.add('poke-name-title')
+        titleElement.innerText = item.name;
+      
+        // creating height in modal content
+        var contentElement = document.createElement('p');
+        contentElement.innerText = 'Pokemon height: ' + item.height;
+
+        // creating img in modal content
+        var renderImg = document.createElement('img');
+        renderImg.src = item.imageUrl
+
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modal.appendChild(renderImg);
+        modalContainer.appendChild(modal);
+      }
+    
     var addListItem = (pokemon) => {
         var pokemonList = document.querySelector('.pokemon-list');
         var listItem = document.createElement('li');
@@ -18,8 +69,9 @@ var pokemonRepository = (() => {
         button.classList.add('poke-btn');
         listItem.appendChild(button);
         pokemonList.appendChild(listItem)
-        button.addEventListener('click',(e)=>{
+        button.addEventListener('click',(e) => {
             showDetails(pokemon);
+            showModal()
         })   
     }
 
@@ -30,6 +82,7 @@ var pokemonRepository = (() => {
           return response.json(); 
         }).then(function (json) {
           json.results.forEach(function (item) {
+        // Ahora de aqui viene el nombre, q agarra el argumento de pokemon que ya esta arriba declarado como pokemon.  
             var pokemon = {
               name: item.name,
               detailsUrl: item.url
@@ -40,17 +93,17 @@ var pokemonRepository = (() => {
           console.error(e);
         })
       }
-
-    // Function to load details for each pokemon:
-
+    
+    // This function takes each pokemon as a item. 
       var loadDetails = (item) => {
         var url = item.detailsUrl;
         return fetch(url).then(function (response) {
           return response.json();
         }).then(function (details) {
           // Now we add the details to the item
-          item.imageUrl = details.sprites.front_default;
-          item.height = details.height;
+          // Aqui podemos empezar a desglozar porque item. porque sabemos los nombre de los keys que posee el objeto.  
+          item.imageUrl = details.sprites.front_default; // analizar este
+          item.height = details.height;   // entender bien de donde salen estos. item.height etc. 
           item.types = details.types;
         }).catch(function (e) {
           console.error(e);
@@ -60,6 +113,7 @@ var pokemonRepository = (() => {
       function showDetails(item) {
         pokemonRepository.loadDetails(item).then(function () {
         console.log(item)
+        showModal(item)
         // console.log('Your pokemon name is: ' + item.name + ' ' +' height ' + item.height + ' Image urld: ' + item.imageUrl);
     });
 }
@@ -80,9 +134,6 @@ pokemonRepository.loadList().then(function() {
         pokemonRepository.addListItem(pokemon);
     });
   });
-
-
-
 
 
 
